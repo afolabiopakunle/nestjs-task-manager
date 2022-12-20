e76f14308d0e3ba8e237e5194f8471dbe4a70c60
Afolabi <opakunleafolabi@gmail.com>
2022-12-20 06:59:46 +0100

    model implemented

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TaskModel } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): TaskModel[] {
     return this.taskService.getAllTasks();
   }
 }
